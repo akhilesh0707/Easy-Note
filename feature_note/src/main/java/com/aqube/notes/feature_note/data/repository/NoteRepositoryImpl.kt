7b494a1e74b65f0d7df60cc51f8d663eb3c0e3bb
Akhilesh <akhilesh0707@gmail.com>
2021-10-03 13:22:35 +0530

    Added NoteRepository and provided an implementation

---
+++ feature_note/src/main/java/com/aqube/notes/feature_note/data/repository/NoteRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.aqube.notes.feature_note.data.repository
+
+import com.aqube.notes.feature_note.data.data_source.NoteDao
+import com.aqube.notes.feature_note.domain.model.Note
+import com.aqube.notes.feature_note.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.Flow
+
+class NoteRepositoryImpl(
+    private val noteDao: NoteDao
+) : NoteRepository {
+    override fun getNotes(): Flow<List<Note>> {
+        return noteDao.getNotes()
+    }
+
+    override suspend fun getNoteById(id: Int): Note? {
+        return noteDao.getNoteById(id)
+    }
+
+    override suspend fun insertNote(note: Note) {
+        noteDao.insertNote(note)
+    }
+
+    override suspend fun deleteNote(note: Note) {
+        noteDao.deleteNote(note)
+    }
+}
