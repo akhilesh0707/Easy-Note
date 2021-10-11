a807a0bfd4630cf84a79a5f4861ebc0d0613b4b1
Akhilesh <akhilesh0707@gmail.com>
2021-10-11 17:56:03 +0530

    Added coroutines and room dependencies object class

---
+++ buildSrc/src/main/java/dependencies/Coroutines.kt
@@ -0,0 +1,7 @@
+package dependencies
+
+object Coroutines {
+    private const val coroutineVersion = "1.5.1"
+    const val coroutineCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion"
+    const val coroutineAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutineVersion"
+}
---
+++ buildSrc/src/main/java/dependencies/Room.kt
@@ -0,0 +1,6 @@
+package dependencies
+
+object Room {
+    private const val roomVersion = "2.3.0"
+    const val roomKtx = "androidx.room:room-ktx:$roomVersion"
+}
