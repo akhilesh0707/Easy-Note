03bffcfdf4609ca3a3075e85bf9540d358bcc83c
Akhilesh <akhilesh0707@gmail.com>
2021-10-11 18:32:08 +0530

    Converted and renamed build.gradle to build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,96 @@
+import dependencies.*
+
+plugins {
+    id(Config.Plugins.androidApplication)
+    id(Config.Plugins.kotlinAndroid)
+    id(Config.Plugins.kotlinKapt)
+    id(Config.Plugins.daggerHiltAndroid)
+}
+
+android {
+    compileSdk = Config.Android.compileSdk
+
+    defaultConfig {
+        minSdk = Config.Android.minSdk
+        targetSdk = Config.Android.targetSdk
+        applicationId = Environments.appId
+        versionCode = Environments.Debug.appVersionCode
+        versionName = Environments.Debug.appVersionName
+
+        testInstrumentationRunner = Config.testInstrumentationRunner
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName(Environments.Debug.name) {
+            //applicationIdSuffix = Environments.Debug.suffix
+            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+        getByName(Environments.Release.name) {
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+        useIR = true
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Compose.composeVersion
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+    // Modules
+    implementation(project(Modules.core))
+    implementation(project(Modules.featureNote))
+
+    // AndroidX dependencies
+    implementation(AndroidX.coreKtx)
+    implementation(AndroidX.appCompat)
+    implementation(AndroidX.lifeCycleKtx)
+    implementation(AndroidX.material)
+
+    // Compose dependencies
+    implementation(Compose.compiler)
+    implementation(Compose.ui)
+    implementation(Compose.material)
+    implementation(Compose.toolingPreview)
+    implementation(Compose.materialIconExtended)
+    implementation(Compose.activityCompose)
+    implementation(Compose.navigationCompose)
+    implementation(Compose.viewModelCompose)
+    implementation(Compose.hiltNavigationCompose)
+
+    // Coroutines
+    implementation(Coroutines.coroutineCore)
+    implementation(Coroutines.coroutineAndroid)
+
+    //Dagger - Hilt
+    implementation(DaggerHilt.hiltAndroid)
+    kapt(DaggerHilt.hiltAndroidCompiler)
+
+    // Kotlin Extensions and Coroutines support for Room
+    implementation(Room.roomKtx)
+}
--- buildSrc/src/main/java/Environments.kt
+++ buildSrc/src/main/java/Environments.kt
@@ -1,12 +1,15 @@
 object Environments {
+    const val appId = "com.aqube.notes"
+
     object Debug {
-        const val appId = "com.aqube.notes.debug"
+        const val name = "debug"
+        const val suffix = ".debug"
         const val appVersionCode = 1
         const val appVersionName = "1.0"
     }
 
     object Release {
-        const val appId = "com.aqube.notes"
+        const val name = "release"
         const val appVersionCode = 1
         const val appVersionName = "1.0"
     }
