83e505e6846a83fc50198d0526b52a7da434dad7
Akhilesh <akhilesh0707@gmail.com>
2021-10-11 17:54:56 +0530

    Converted app.gradle and added separate dependencies object

---
+++ buildSrc/src/main/java/dependencies/AndroidX.kt
@@ -0,0 +1,8 @@
+package dependencies
+
+object AndroidX {
+    const val coreKtx = "androidx.core:core-ktx:1.6.0"
+    const val appCompat = "androidx.appcompat:appcompat:1.3.1"
+    const val lifeCycleKtx = "androidx.lifecycle:lifecycle-runtime-ktx:2.3.1"
+    const val material = "com.google.android.material:material:1.4.0"
+}
