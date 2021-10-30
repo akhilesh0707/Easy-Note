8d27ffdc1844c68a3df3d1da0803ea8fd02e22df
Akhilesh <akhilesh0707@gmail.com>
2021-10-30 13:01:49 +0530

    Included theme and app version into settings screens

--- feature_settings/src/main/java/com/aqube/notes/feature_settings/presentation/SettingsScreen.kt
+++ feature_settings/src/main/java/com/aqube/notes/feature_settings/presentation/SettingsScreen.kt
@@ -1,53 +1,50 @@
 package com.aqube.notes.feature_settings.presentation
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import android.content.Context
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.aqube.notes.core.presentation.components.DarkModeToggleButton
+import com.aqube.notes.core.presentation.components.LabelView
+import com.aqube.notes.core.presentation.theme.NoteAppTheme
+import com.aqube.notes.core.util.getVersionName
+import com.aqube.notes.feature_settings.presentation.components.theme.DarkThemeRow
+import com.aqube.notes.feature_settings.presentation.components.version.VersionRow
 
 @Composable
 fun SettingsScreen(
     darkTheme: Boolean,
     onToggleTheme: (Boolean) -> Unit,
     upPress: () -> Unit,
-    viewModel: SettingsViewModel = hiltViewModel()
+    context: Context = LocalContext.current
 ) {
-    Column(
+    LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
+            .padding(16.dp),
+
         ) {
-            Text(
-                text = "Dark theme",
-                style = MaterialTheme.typography.h5,
-                color = MaterialTheme.colors.onPrimary
-            )
-            DarkModeToggleButton(
-                darkMode = darkTheme,
-                onDarkModeChanged = { toggleValue ->
-                    onToggleTheme(toggleValue)
-                },
-                modifier = Modifier.weight(1f)
+        item { LabelView(title = "Settings") }
+        item {
+            DarkThemeRow(
+                label = "Dark theme",
+                darkTheme = darkTheme,
+                onToggleTheme = onToggleTheme
             )
         }
+        item { VersionRow(label = "Application version", context.getVersionName()) }
     }
+
 }
 
-@Preview
+@Preview(name = "Preview", group = "Size")
 @Composable
-fun ComposablePreview() {
-    SettingsScreen(darkTheme = true,onToggleTheme = { },{})
+fun SettingsPreview() {
+    NoteAppTheme() {
+        SettingsScreen(darkTheme = false, onToggleTheme = { }, {}, LocalContext.current)
+    }
 }
