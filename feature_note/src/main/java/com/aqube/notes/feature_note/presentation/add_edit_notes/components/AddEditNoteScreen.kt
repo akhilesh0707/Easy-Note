46d18ae9c08509bea9f1246949de49ac35891049
Akhilesh <akhilesh0707@gmail.com>
2021-11-05 04:32:53 +0530

    Note add edit screen changes

--- feature_note/src/main/java/com/aqube/notes/feature_note/presentation/add_edit_notes/components/AddEditNoteScreen.kt
+++ feature_note/src/main/java/com/aqube/notes/feature_note/presentation/add_edit_notes/components/AddEditNoteScreen.kt
@@ -1,39 +1,25 @@
 package com.aqube.notes.feature_note.presentation.add_edit_notes.components
 
 import androidx.compose.animation.Animatable
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.aqube.notes.core.presentation.components.TextInputField
-import com.aqube.notes.core.presentation.theme.PaleBlack
-import com.aqube.notes.core.presentation.theme.White
-import com.aqube.notes.core.presentation.theme.Yellow600
+import com.aqube.notes.core.presentation.components.ScaffoldWithFAB
 import com.aqube.notes.feature_note.R
-import com.aqube.notes.feature_note.domain.model.Note
 import com.aqube.notes.feature_note.presentation.add_edit_notes.AddEditNoteEvent
 import com.aqube.notes.feature_note.presentation.add_edit_notes.AddEditNoteViewModel
 import com.aqube.notes.feature_note.presentation.add_edit_notes.UiEvent
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 
 @ExperimentalComposeUiApi
 @Composable
@@ -42,10 +28,8 @@ fun AddEditNoteScreen(
     noteColor: Int,
     viewModel: AddEditNoteViewModel = hiltViewModel()
 ) {
-    val titleState = viewModel.title.value
-    val contentState = viewModel.content.value
+
     val scaffoldState = rememberScaffoldState()
-    val scope = rememberCoroutineScope()
 
     val noteBackgroundAnimatable = remember {
         Animatable(Color(if (noteColor != -1) noteColor else viewModel.color.value))
@@ -64,23 +48,13 @@ fun AddEditNoteScreen(
         }
     }
 
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
-                    viewModel.onEvent(AddEditNoteEvent.SaveNote)
-                },
-                backgroundColor = Yellow600
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_save),
-                    modifier = Modifier.size(24.dp),
-                    contentDescription = stringResource(id = R.string.add_update_note),
-                    tint = White
-                )
-            }
-        },
-        scaffoldState = scaffoldState
+    ScaffoldWithFAB(
+        label = stringResource(id = R.string.add_update_note),
+        icon = painterResource(id = R.drawable.ic_save),
+        scaffoldState = scaffoldState,
+        onClick = {
+            viewModel.onEvent(AddEditNoteEvent.SaveNote)
+        }
     ) {
         Column(
             modifier = Modifier
@@ -88,56 +62,9 @@ fun AddEditNoteScreen(
                 .background(noteBackgroundAnimatable.value)
                 .padding(16.dp)
         ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            )
-            {
-                Note.noteColors.forEach { color ->
-                    val colorInt = color.toArgb()
-                    Box(
-                        modifier = Modifier
-                            .size(50.dp)
-                            .shadow(10.dp)
-                            .background(color)
-                            .border(
-                                width = 1.dp,
-                                color = if (viewModel.color.value == colorInt)
-                                    Color.Black
-                                else
-                                    Color.Transparent,
-                            )
-                            .clickable {
-                                scope.launch {
-                                    noteBackgroundAnimatable.animateTo(
-                                        targetValue = Color(colorInt),
-                                        animationSpec = tween(500)
-                                    )
-                                }
-                                viewModel.onEvent(AddEditNoteEvent.ChangeColor(colorInt))
-                            }
-                    )
-
-                }
-            }
+            ColorSelectorItem(viewModel = viewModel, noteBackgroundAnim = noteBackgroundAnimatable)
             Spacer(modifier = Modifier.height(24.dp))
-
-            TextInputField(
-                label = titleState.hint,
-                value = titleState.text,
-                color = PaleBlack,
-                onValueChanged = { viewModel.onEvent(AddEditNoteEvent.EnteredTitle(it)) }
-            )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            TextInputField(
-                label = contentState.hint,
-                value = contentState.text,
-                color = PaleBlack,
-                singleLine = false,
-                onValueChanged = { viewModel.onEvent(AddEditNoteEvent.EnteredContent(it)) }
-            )
+            AddEditNoteItem(viewModel = viewModel)
         }
     }
 
