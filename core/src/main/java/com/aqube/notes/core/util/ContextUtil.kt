b8be037db7df36d49ef780dd1b7983f1f3fa4590
Akhilesh <akhilesh0707@gmail.com>
2021-11-05 04:29:02 +0530

    Handled view intent for profile clicks

--- core/src/main/java/com/aqube/notes/core/util/ContextUtil.kt
+++ core/src/main/java/com/aqube/notes/core/util/ContextUtil.kt
@@ -1,5 +1,18 @@
 package com.aqube.notes.core.util
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 
 fun Context.getVersionName(): String = packageManager.getPackageInfo(packageName, 0).versionName
+
+
+inline fun Context.openWebPage(url: String, onCantHandleAction: () -> Unit = {}) {
+    val webpage: Uri = Uri.parse(url)
+    val intent = Intent(Intent.ACTION_VIEW, webpage)
+    try {
+        startActivity(intent)
+    } catch (t: Throwable) {
+        onCantHandleAction()
+    }
+}
