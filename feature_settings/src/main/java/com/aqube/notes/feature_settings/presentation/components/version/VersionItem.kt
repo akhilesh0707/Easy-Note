418d59ab6c22020aedea35e571fe9723dd0c5e4c
Akhilesh <akhilesh0707@gmail.com>
2021-11-05 04:31:46 +0530

    Minor changes for version item

--- feature_settings/src/main/java/com/aqube/notes/feature_settings/presentation/components/version/VersionItem.kt
+++ feature_settings/src/main/java/com/aqube/notes/feature_settings/presentation/components/version/VersionItem.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.aqube.notes.core.R
@@ -32,6 +33,6 @@ fun AppVersionNumber(appVersion: String) {
         modifier = Modifier.padding(start = 8.dp, end = 16.dp),
         text = appVersion,
         style = MaterialTheme.typography.h5,
-        color = MaterialTheme.colors.onPrimary
+        color = Color.Gray
     )
 }
