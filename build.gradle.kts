3cb25bb9a962c0c630a32b70a44c3e1959641ece
Akhilesh <akhilesh0707@gmail.com>
2021-11-05 12:37:25 +0530

    Minor changes on settings screen

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ buildscript {
         classpath(Config.ClassPath.gradleBuildTools)
         classpath(Config.ClassPath.kotlinGradlePlugin)
         classpath(Config.ClassPath.hiltGradlePlugin)
-
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
